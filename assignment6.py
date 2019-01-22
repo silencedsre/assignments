64b66636397738c964a947d19f414904f9918757
silencedsre <skr3sna@gmail.com>
2019-01-22 21:08:04 +0545

    Add files via upload

--- assignment6.py
+++ assignment6.py
@@ -1,15 +1,57 @@
-#Generate a list of the first Fibonacci numbers,
-#0 being the first number. Then, apply the map function and a lambda expression 
-#to cube each fibonacci number and print the list.
+def function_fac(n):
+    if n==0:
+        fac=1
+        print(fac)
+    else:
+        fac=n
+        while n>1:
+            fac=fac*(n-1)
+            n=n-1
+        print("factorial is: "+str(fac))
+anynum=int(input("enter any num for factorial:"))
+function_fac(anynum)
 
-list_fabi=[0,1]
-n=int(input("enter a num for total fab series: "))
+def function_max(x):
+    # print(x)
+    i=0
+    for items in x:
+        if items>x[i]:
+            i+=1
+    return items
+
+num=int(input("how many no: "))
 i=0
-while i<n-2:
-    fabi=list_fabi[-1]+list_fabi[-2]
-    list_fabi.append(fabi)
-    i+=1
-print(list_fabi)
+list1=[]
+while i<num:
+   inputs=int(input("enter the no: "))
+   list1.append(inputs)
+   i=i+1
+print("max of them is: " +str(function_max(list1)))
 
-map_obj=map(lambda x:x**3, list_fabi)
-print(list(map_obj))
+import re
+def funtion_string(string1):
+    alphabets=re.findall('\S',string1)
+    return alphabets
+any_string=input("enter a string lower and uppercase:")
+list_alphabets=funtion_string(any_string)
+lower=0
+upper=0
+for alphabet in list_alphabets:
+    if alphabet.islower():
+        lower+=1
+    else:
+        upper+=1
+print("lower is "+str(lower)+" upper is "+ str(upper))
+
+import re
+def function_string(list_hypen1):
+    list_hypen1.sort()
+    string_sorted='-'.join(list_hypen1)
+    print(string_sorted)
+
+
+input_hypen=input("Enter hypen seperated string:")
+list_hypen=input_hypen.split("-")
+# list_hypen=re.findall('\S+?-',input_hypen)
+# print(list_hypen)
+function_string(list_hypen)
